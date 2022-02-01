bf012858c0f53f9736753d5d784a288d68f4f4e9
Michael Dever <mdever@live.ca>
2022-02-01 16:22:20 -0500

    exercise 5 complete

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,6 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "Total revenue: #{Store.sum("annual_revenue")}"
+puts "Average revenue: #{Store.sum("annual_revenue")/Store.count}"
+puts "Stores over $1000000: #{Store.where("annual_revenue > 1000000").count}"
